e41a9c885f18d012ef293eb8d70dba962bb58e0f
Samuel Conner <prince@Samuels-MacBook-Pro.local>
2021-04-01 05:00:51 -0600

    Added styling for head and footer

--- ethdapp/src/app/app.module.ts
+++ ethdapp/src/app/app.module.ts
@@ -7,6 +7,15 @@ import { TransferComponent } from './components/transfer/transfer.component';
 import { StartComponent } from './components/start/start.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,9 +26,16 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+BrowserModule,
+AppRoutingModule,
+BrowserAnimationsModule,
+MatButtonModule,
+MatInputModule,
+MatDatepickerModule,
+MatNativeDateModule,
+MatCheckboxModule,
+MatSelectModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
