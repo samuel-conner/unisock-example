52eddf23166244c117a9957ba204eea59c972e30
Samuel Conner <prince@Samuels-MacBook-Pro.local>
2021-04-08 11:37:23 -0600

    working on web3

---
+++ ethdapp/truffle/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Transfer = artifacts.require("/Users/prince/Code/unisock-example/ethdapp/truffle/contracts/unisock.vy");
+module.exports = function(deployer) {
+  deployer.deploy(Transfer);
+};
+// /Users/prince/Code/unisock-example/ethdapp/truffle/contracts/unisock.vy
---
+++ ethdapp/truffle/truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 5000000,
+      gasPrice: 100000000000
+    }
+  },
+  compilers: {
+    vyper: { "language": "Vyper"
+      // settings: {
+      //   evmVersion: "byzantium"
+      // }
+    }
+  }
+};
