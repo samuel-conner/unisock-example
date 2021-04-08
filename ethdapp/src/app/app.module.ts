fef51e0d4e0e51c17d1a13a26ec7f368ddfbed16
Samuel Conner <prince@Samuels-MacBook-Pro.local>
2021-04-08 06:51:12 -0600

    font end for dapp done, validations need some work

--- ethdapp/src/app/app.module.ts
+++ ethdapp/src/app/app.module.ts
@@ -8,14 +8,14 @@ import { StartComponent } from './components/start/start.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatInputModule,
-  MatSelectModule,
-  MatDatepickerModule,
-  MatNativeDateModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -34,7 +34,9 @@ MatInputModule,
 MatDatepickerModule,
 MatNativeDateModule,
 MatCheckboxModule,
-MatSelectModule
+MatSelectModule,
+FormsModule,
+ReactiveFormsModule,
 ],
   providers: [],
   bootstrap: [AppComponent]
